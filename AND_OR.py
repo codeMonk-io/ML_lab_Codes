c97055f1a6db228ac8d26c845bf5f3c29f881424
Shubham Raj <YOUR_GITHUB_EMAIL>
2026-06-13 17:03:23 +0530

    And or

--- AND_OR.py
+++ AND_OR.py
@@ -10,27 +10,17 @@ def perceptron(x1, x2, w1, w2, threshold):
 
 
 # Input combinations
-inputs = [
-    (0,0),
-    (0,1),
-    (1,0),
-    (1,1)
+inputs = [ (0,0), (0,1), (1,0), (1,1)
 ]
 
 print("AND Gate")
 for x1, x2 in inputs:
-    output = perceptron(x1, x2,
-                        w1=1,
-                        w2=1,
-                        threshold=2)
+    output = perceptron(x1, x2, w1=1, w2=1, threshold=2)
 
     print(x1, x2, "->", output)
 
 print("\nOR Gate")
 for x1, x2 in inputs:
-    output = perceptron(x1, x2,
-                        w1=1,
-                        w2=1,
-                        threshold=1)
+    output = perceptron(x1, x2, w1=1, w2=1, threshold=1)
 
     print(x1, x2, "->", output)
